78b46773647667af0491e669c4e4d303c22a2d03
Serge Paquet <srgpqt@gmail.com>
2020-10-29 09:28:02 -0400

    Remove silly i() function

--- lib/briefly/entry.ex
+++ lib/briefly/entry.ex
@@ -68,7 +68,7 @@ defmodule Briefly.Entry do
 
   defp ensure_tmp_dir(tmps) do
     {mega, _, _} = :os.timestamp()
-    subdir = "briefly-" <> i(mega)
+    subdir = "briefly-#{mega}"
     Enum.find_value(tmps, &write_tmp_dir(Path.join(&1, subdir)))
   end
 
@@ -109,9 +109,6 @@ defmodule Briefly.Entry do
   defp open(_prefix, tmp, attempts, _pid, _ets, _paths) do
     {:too_many_attempts, tmp, attempts}
   end
-
-  @compile {:inline, i: 1}
-  defp i(integer), do: Integer.to_string(integer)
 
   defp path(options, tmp) do
     time = :erlang.monotonic_time() |> to_string |> String.trim("-")
